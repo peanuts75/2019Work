ee14561a46053c60c8728302148f3834d2401a95
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-10-29 09:43:35 +0000

    added the converter

---
+++ Portfolio/js/converter.js
@@ -0,0 +1,1 @@
+const conversion = [document.getElementById("numtype"), document.getElementById("tofrom")]
---
+++ W5/React/main.js
@@ -0,0 +1,10 @@
+class FAQQ extends React.Component {
+    render() {
+        return (
+            <div>example</div>
+        )
+    }
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<FAQQ />, rootElement)
